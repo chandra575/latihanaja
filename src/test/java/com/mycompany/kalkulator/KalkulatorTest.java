836250194dd2aaa1709943723d1eb7e176de2227
chandra575 <67907359+chandra575@users.noreply.github.com>
2024-06-04 19:18:41 +0700

    Add files via upload

---
+++ src/test/java/com/mycompany/kalkulator/KalkulatorTest.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.mycompany.kalkulator;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author c-law
+ */
+public class KalkulatorTest {
+    
+    @Test
+    public void testBEqualsZero() {
+        assertEquals(1, Kalkulator.CariNilaiPangkat(2, 0));
+        assertEquals(1, Kalkulator.CariNilaiPangkat(0, 0));
+    }
+
+    @Test
+    public void testBNegative() {
+        assertEquals(-1, Kalkulator.CariNilaiPangkat(2, -3));
+    }
+
+    @Test
+    public void testBGreaterThanTen() {
+        assertEquals(-2, Kalkulator.CariNilaiPangkat(2, 11));
+    }
+
+    @Test
+    public void testAGreaterThanHundred() {
+        assertEquals(-2, Kalkulator.CariNilaiPangkat(101, 3));
+    }
+
+    @Test
+    public void testValidPangkat() {
+        assertEquals(8, Kalkulator.CariNilaiPangkat(2, 3));
+        assertEquals(16, Kalkulator.CariNilaiPangkat(2, 4));
+    }
+    
+}
